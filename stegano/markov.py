fc26893ce5403098cbf272c8b8b0b81cc4b83c6f
trashman <RemiRosenthal@users.noreply.github.com>
2019-04-14 18:22:03 +0100

    Define StateTransitions class. Add transition function. Validate for loops.

--- stegano/markov.py
+++ stegano/markov.py
@@ -1,8 +1,11 @@
-from typing import Tuple
+import random
+from itertools import accumulate
+from typing import Tuple, Set
 
 State = str
 Probability = float
-Transition = Tuple[State, State, Probability]
+Transition = Tuple[State, Probability]
+Transitions = Set[Transition]
 
 START_STATE_LABEL = "s0"
 
@@ -12,38 +15,66 @@ class MarkovError(Exception):
     pass
 
 
+class StateTransitions:
+    """
+    A set of outbound transitions for a state is a non-empty set of elements, each with a unique and valid state
+    label and non-zero probability.
+    """
+
+    def __init__(self, transitions: Transitions):
+        input_transitions = dict((label, prob) for label, prob in transitions)
+        self.transitions = {}
+        for key, value in input_transitions.items():
+            if value > 0:
+                self.transitions[key] = value
+
+
 class MarkovChain:
     """
-    A Markov chain is a collection of states with unique labels, and transitions between those states. There are a
-    number of constraints.
+    A Markov chain is a collection of states with unique labels, and state transitions defining the outbound transitions
+    for every state. There are a number of constraints on the chain.
 
     * It must have a start state, labelled as s0.
     * It must have at least one other state.
-    * All states must have at least one inbound transition so that all states are reachable.
-    * All states must have at least one outbound transition so that the chain forms a cycle through s0.
     * Any state except s0 may have a transition to s0.
     * Ignoring all transitions to s0, the chain must have no cycles.
     * No transition has probability 0.
+
+    It is possible that the Markov chain is made up of multiple connected components and is not fully connected,
+    so not all states are guaranteed to be reachable from s0. Care should be taken when defining transitions.
     """
 
     def __init__(self, states: set):
         self.states = states
         if not self.states.__contains__(START_STATE_LABEL):
             self.states.add(START_STATE_LABEL)
-        self.transitions = set()
         self.current_state = START_STATE_LABEL
+        self.markov_chain = {}
+        for state in self.states:
+            self.markov_chain.update({state: None})
 
-    def set_transitions(self, transitions: set):
+    def set_transitions(self, transitions: Transitions):
         """
-        Set the transitions for this Markov chain. The chain will then be validated.
+        Validate and set transitions for this Markov chain from the given set. Check for cycles in the chain.
         :param transitions: A set of transitions, which are 3-tuples (State, State, Probability).
         """
+        self.validate_transitions(transitions)
+
+        for from_state, to_state, prob in transitions:
+            outbound_transitions = self.markov_chain.get(from_state)
+            if outbound_transitions is None:
+                self.markov_chain.update({from_state: StateTransitions({(to_state, prob)})})
+            else:
+                self.markov_chain.get(from_state).transitions.update({to_state: prob})
+        self.find_cycles()
+
+    def validate_transitions(self, transitions: Transitions):
         error_messages = set()
         error_states = set()
         error_probabilities = set()
         error_transitions = set()
-        s0_inward = 0
         s0_outward = 0
+        s0_inward = 0
         for from_state, to_state, prob in transitions:
             if from_state not in self.states:
                 error_states.add(from_state)
@@ -57,8 +88,6 @@ class MarkovChain:
                 s0_outward += 1
             elif to_state.__eq__(START_STATE_LABEL):
                 s0_inward += 1
-        # TODO check s0 transitions; find loops
-
         if error_states.__len__() > 0:
             error_messages.add("States in the given transitions were not found in the Markov chain: {}".
                                format(error_states))
@@ -66,24 +95,50 @@ class MarkovChain:
             error_messages.add("Some probabilities in the given transitions were invalid: {}".
                                format(error_probabilities))
         if error_transitions.__len__() > 0:
-            error_messages.add("Some of the given transitions caused a loop: {}".
+            error_messages.add("States in the given transitions pointed to themselves: {}".
                                format(error_transitions))
         if s0_inward == 0:
             error_messages.add("The start state had no inward transitions.")
         if s0_outward == 0:
             error_messages.add("The start state had no outward transitions.")
-
         if error_messages.__len__() > 0:
             raise MarkovError("The given transitions were invalid: \n{}".format(error_messages))
 
-        self.transitions = transitions
+    def find_cycles(self):
+        walk = ["s0"]
+        index = 0
 
-    def transition(self):
-        outbound_transitions = set()
+        while index < len(walk):
+            transitions = self.markov_chain.get(walk[index]).transitions
+            # if transitions.get(walk[index]) is not None:
+            #     raise MarkovError("Found a loop at state {}".format(walk[index]))
+            for outbound_state in transitions.keys():
+                if not outbound_state.__eq__("s0"):
+                    if outbound_state in walk[:index + 1]:
+                        raise MarkovError("Found a loop at transition {} to {}".format(walk[index], outbound_state))
+                    else:
+                        walk.append(outbound_state)
+            index += 1
 
     def get_outbound_transitions(self) -> (State, Probability):
-        outbound_transitions = set()
-        for from_state, to_state, prob in self.transitions:
-            if from_state.__eq__(self.current_state):
-                outbound_transitions.add((to_state, prob))
+        """
+        :return: the outbound transitions for the current state as a dictionary of Transition types
+        """
+        # noinspection PyTypeChecker
+        outbound_transitions: StateTransitions = self.markov_chain.get(self.current_state)
         return outbound_transitions
+
+    def accumulate_transitions(self) -> list:
+        outbound_transitions = self.get_outbound_transitions()
+        accum = zip(outbound_transitions.transitions.keys(), accumulate(outbound_transitions.transitions.values()))
+        return list(accum)
+
+    def transition(self):
+        accum = self.accumulate_transitions()
+        total = accum[len(accum) - 1][1]
+        rand = random.uniform(0, total)
+        for state, prob in accum:
+            if rand <= prob:
+                print("Transitioning to {}".format(state))
+                self.current_state = state
+                return
--- tests/test_markov.py
+++ tests/test_markov.py
@@ -1,6 +1,6 @@
 import unittest
 
-from stegano.markov import MarkovChain, MarkovError
+from stegano.markov import MarkovChain, MarkovError, StateTransitions
 
 
 class TestMarkovChain(unittest.TestCase):
@@ -12,7 +12,8 @@ class TestMarkovChain(unittest.TestCase):
     def test_init(self):
         self.assertSetEqual(self.test_states, self.markov_chain.states)
         self.assertEqual("s0", self.markov_chain.current_state)
-        self.assertSetEqual(set(), self.markov_chain.transitions)
+        self.assertDictEqual({"s0": None, "s1": None, "s2": None, "s3": None, "s4": None},
+                             self.markov_chain.markov_chain)
 
     def test_init_s0(self):
         self.markov_chain = MarkovChain(self.states.union({"s0"}))
@@ -36,7 +37,9 @@ class TestMarkovChainTransitions(unittest.TestCase):
 
     def test_set_transitions(self):
         self.markov_chain.set_transitions(self.transitions)
-        self.assertSetEqual(self.transitions, self.markov_chain.transitions)
+        self.assertEqual(5, len(self.markov_chain.markov_chain))
+        self.assertIsInstance(self.markov_chain.markov_chain.get("s0"), StateTransitions)
+        self.assertDictEqual({"s1": 2, "s2": 3}, self.markov_chain.markov_chain.get("s0").transitions)
 
     def test_set_transitions_bad_outbound(self):
         self.transitions.add(("s4", "s5", 1))
@@ -54,16 +57,58 @@ class TestMarkovChainTransitions(unittest.TestCase):
         self.transitions.add(("s3", "s4", 0))
         self.assertRaises(MarkovError, self.markov_chain.set_transitions, self.transitions)
 
-    def test_get_outbound_states(self):
+    def test_set_transitions_s0_no_outbound(self):
+        self.transitions = {x for x in self.transitions if not x[0].__eq__("s0")}
+        self.assertRaises(MarkovError, self.markov_chain.set_transitions, self.transitions)
+
+    def test_set_transitions_s0_no_inbound(self):
+        self.transitions = {x for x in self.transitions if not x[1].__eq__("s0")}
+        self.assertRaises(MarkovError, self.markov_chain.set_transitions, self.transitions)
+
+    def test_get_outbound_transitions(self):
         self.markov_chain.set_transitions(self.transitions)
-        outbound_states = self.markov_chain.get_outbound_transitions()
-        expected = {("s1", 2), ("s2", 3)}
-        self.assertSetEqual(expected, outbound_states)
+        self.markov_chain.current_state = "s0"
+        outbound_transitions = self.markov_chain.get_outbound_transitions()
+        self.assertIsInstance(outbound_transitions, StateTransitions)
+        self.assertEqual(2, len(outbound_transitions.transitions))
+        self.assertEqual(2, outbound_transitions.transitions.get("s1"))
+        self.assertEqual(3, outbound_transitions.transitions.get("s2"))
 
         self.markov_chain.current_state = "s4"
-        outbound_states = self.markov_chain.get_outbound_transitions()
-        expected = {("s0", 2)}
-        self.assertSetEqual(expected, outbound_states)
+        outbound_transitions = self.markov_chain.get_outbound_transitions()
+        self.assertIsInstance(outbound_transitions, StateTransitions)
+        self.assertEqual(1, len(outbound_transitions.transitions))
+        self.assertEqual(2, outbound_transitions.transitions.get("s0"))
+
+    def test_accumulate_transitions(self):
+        self.markov_chain.set_transitions(self.transitions)
+        self.markov_chain.current_state = "s0"
+        accum = self.markov_chain.accumulate_transitions()
+        self.assertIsInstance(accum, list)
+        self.assertEqual(2, len(accum))
+        self.assertTrue(any(x[1] == 5) for x in accum)
+
+    def test_transition(self):
+        self.markov_chain.set_transitions(self.transitions)
+        self.markov_chain.current_state = "s0"
+        self.markov_chain.transition()
+        self.assertIn(self.markov_chain.current_state, {"s1", "s2"})
+
+        self.markov_chain.transition()
+        self.assertIn(self.markov_chain.current_state, {"s3", "s4"})
+
+        self.markov_chain.transition()
+        self.assertEqual("s0", self.markov_chain.current_state)
+
+    def test_find_2_cycle(self):
+        self.transitions.add(("s4", "s2", 1))
+        self.assertRaises(MarkovError, self.markov_chain.set_transitions, self.transitions)
+
+    def test_find_3_cycle(self):
+        self.transitions = {x for x in self.transitions if not x[1].__eq__("s3")}
+        self.transitions.add(("s4", "s3", 1))
+        self.transitions.add(("s3", "s1", 1))
+        self.assertRaises(MarkovError, self.markov_chain.set_transitions, self.transitions)
 
 
 if __name__ == '__main__':
