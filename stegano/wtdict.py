0564556eb0da83e4a3a212575666d99d8e0f8ae0
trashman <RemiRosenthal@users.noreply.github.com>
2019-05-01 17:01:00 +0100

    Reformat wtdict

--- stegano/wtdict.py
+++ stegano/wtdict.py
@@ -1,5 +1,4 @@
 import json
-from collections import namedtuple
 from typing import Tuple, Set, Dict
 
 from bitstring import Bits
@@ -12,12 +11,14 @@ DEFAULT_DICT_FILE = "..\\sample\\word_type_dict.json"
 
 class MappingDictionary:
     """
-    A mapping dictionary is defined uniquely by a single word-type and has a non-empty set of mappings between symbols
-    and their bit strings.
+    A mapping dictionary is defined uniquely by a single word-type and
+    has a non-empty set of mappings between symbols and their
+    bit-strings.
     """
 
     def __init__(self, mappings: Mappings, encode_spaces=True):
-        input_mappings = dict((string, bits) for string, bits in mappings)
+        input_mappings = dict(
+            (string, bits) for string, bits in mappings)
         self.mappings = {}
         for key, value in input_mappings.items():
             if value not in self.mappings.values():
@@ -60,14 +61,12 @@ class WordTypeDictionary:
             serial_dict.update({mapping_key: mapping_dict.__dict__()})
         return serial_dict
 
-    @staticmethod
-    def from_dict(wt_dict: str):
-        json.loads(wt_dict, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-
     def append_word_type(self, input_dict: WTDict):
         """
-        Add to this word-type dictionary the mappings of another dictionary. They each must have a unique word-type
-        name and symbols which are unique to the dictionary.
+        Add to this word-type dictionary the mappings of another
+        dictionary. They each must have a unique word-type name and
+        symbols which are unique to the dictionary.
+
         :param input_dict: the dictionary of new word lists.
         """
         input_keys = input_dict.keys()
@@ -77,25 +76,32 @@ class WordTypeDictionary:
 
             mapping_dict = self.wt_dict.get(key)
 
-            duplicate_values = self.contains_any_words_from_set(set(input_dict.get(key).mappings.items()), key)
+            duplicate_values = self.contains_any_words_from_set(
+                set(input_dict.get(key).mappings.items()), key)
             if duplicate_values.__len__() > 0:
                 print("Dictionary contained duplicate values:")
             for duplicate_value in duplicate_values:
-                print("{} ({})".format(duplicate_value[0], duplicate_value[1]))
+                print("{} ({})".format(
+                    duplicate_value[0], duplicate_value[1]))
                 del input_dict.get(key).mappings[duplicate_value[0]]
 
             if mapping_dict is None:
                 self.wt_dict.update({key: input_dict.get(key)})
             else:
-                mapping_dict.mappings.update(input_dict.get(key).mappings)
+                mapping_dict.mappings.update(
+                    input_dict.get(key).mappings)
 
-    def contains_any_words_from_set(self, pairs: set, exclude_key=None) -> Set[Tuple[str, str]]:
+    def contains_any_words_from_set(self, pairs: set,
+                                    exclude_key=None) -> Set[
+        Tuple[str, str]]:
         """
-        Look through this word type dictionary. Return the subset of the input set of pairs whose value exists in this
+        Look through this word type dictionary. Return the subset of
+        the input set of pairs whose value exists in this
         dictionary under any word-type.
 
         :param pairs: the set of key-value pairs to check
-        :param exclude_key: the key to exclude from the search of the dictionary
+        :param exclude_key: the key to exclude from the search of the
+        dictionary
         :return: a subset of pairs
         """
         words = set(key for key, value in pairs)
@@ -109,11 +115,13 @@ class WordTypeDictionary:
 
     def remove_word_type(self, word_types: set):
         """
-        Remove from this word-type dictionary every word list that is specified by a word-type name in the given set.
+        Remove from this word-type dictionary every word list that is
+        specified by a word-type name in the given set.
 
         Any invalid elements in the set are ignored.
-        :param word_types: the set of word-types whose lists should be removed from the dictionary
 
+        :param word_types: the set of word-types whose lists should be
+        removed from the dictionary
         """
         if len(word_types) == 0:
             return
@@ -125,9 +133,13 @@ class WordTypeDictionary:
 
     def remove_word(self, word_types: set):
         """
-        Remove from this word-type dictionary every word that is specified by in the given set.
+        Remove from this word-type dictionary every word that is
+        specified by in the given set.
+
         Any invalid elements in the set are ignored.
-        :param word_types: the set of words that should be removed from the dictionary
+
+        :param word_types: the set of words that should be removed
+        from the dictionary
 
         """
         if len(word_types) == 0:
@@ -144,7 +156,8 @@ class WordTypeDictionary:
 
     def generate_state_definitions(self) -> list:
         """
-        From this word-type dictionary, generate a list of distinct states identified uniquely by their word-type name.
+        From this word-type dictionary, generate a list of distinct
+        states identified uniquely by their word-type name.
 
         This list does not explicitly include a start state.
 
@@ -155,7 +168,8 @@ class WordTypeDictionary:
 
 def deserialise_dict(wt_dict: dict) -> WTDict:
     """
-    Convert a serialised dict (of strings) to a WTDict (of mapping objects).
+    Convert a serialised dict (of strings) to a WTDict (of mapping
+    objects).
 
     :param wt_dict: serialised dict
     :return: a WTDict
@@ -163,8 +177,11 @@ def deserialise_dict(wt_dict: dict) -> WTDict:
     deserialised_dict = {}
     for mapping_key in wt_dict.keys():
         mapping_dict = wt_dict.get(mapping_key)
-        mappings = {(x, Bits(bin=y)) for x, y in mapping_dict.get("mappings").items()}
-        deserialised_dict.update({mapping_key: MappingDictionary(mappings, mapping_dict.get("encode_spaces"))})
+        mappings = {(x, Bits(bin=y)) for x, y in
+                    mapping_dict.get("mappings").items()}
+        deserialised_dict.update({
+            mapping_key: MappingDictionary(
+                mappings, mapping_dict.get("encode_spaces"))})
     return deserialised_dict
 
 
@@ -184,7 +201,8 @@ def load_dict(dict_filename=DEFAULT_DICT_FILE) -> WordTypeDictionary:
         print("Could not read dictionary file " + dict_filename)
 
 
-def save_dict(dictionary: WordTypeDictionary, dict_filename=DEFAULT_DICT_FILE):
+def save_dict(dictionary: WordTypeDictionary,
+              dict_filename=DEFAULT_DICT_FILE):
     """
     Save a word-type dictionary object as a JSON file.
 
@@ -193,6 +211,7 @@ def save_dict(dictionary: WordTypeDictionary, dict_filename=DEFAULT_DICT_FILE):
     """
     try:
         with open(dict_filename, "w", encoding="utf-8") as handle:
-            json.dump(dictionary, handle, indent=2, default=lambda o: o.__dict__())
+            json.dump(dictionary, handle, indent=2,
+                      default=lambda o: o.__dict__())
     except IOError:
         print("Could not write dictionary file " + dict_filename)
